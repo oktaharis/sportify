a8f32689a80f7b80719958f8a3c5c6706928857b
oktaharis <oktaharis2008@gmail>
2025-04-29 11:04:40 +0700

    Bugfix: deploy part 3

--- api/index.js
+++
@@ -1,4 +0,0 @@
-// api/index.js
-const serverless = require('serverless-http');
-const app = require('../server.js');     // impor Express app yang diekspor
-module.exports = serverless(app);     // bungkus jadi serverless function
